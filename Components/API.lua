f430f68ba000ebf635dc5c253979c73bc67a9d24
veechs <129508963+veechs@users.noreply.github.com>
2025-01-01 20:04:00 -0500

    Update AddRuleFunction() comments with example

--- Components/API.lua
+++ Components/API.lua
@@ -4,7 +4,9 @@
 Bagshui:LoadComponent(function()
 
 
---- Add a new rule function to Bagshui.
+--- Add a new rule function to Bagshui. A simple example is below. The parameters
+--- use the same basic format as the built-in rule functions, so you can refer
+--- to Config\RuleFunctions.lua for more examples.
 ---
 --- `functionNames` notes: For aliases, the first item in the array is the primary name, and all subsequent items are aliases.
 --- 
@@ -20,9 +22,32 @@ Bagshui:LoadComponent(function()
 ---
 --- `ruleTemplates` notes: `code` is the menu text and what will be inserted in the editor; `description` will be in the tooltip.
 ---
+--- # Example
+--- ```
+---	Bagshui:AddRuleFunction(
+---		{
+---			"IsSolidStone",
+---			"Stone"
+---		},
+---		function(rules, ruleArguments)
+---			if rules.item.name == "Solid Stone" then
+---				return true
+---			end
+---
+---			return false
+---		end,
+---		nil,
+---		{
+---			{
+---				code = 'IsSolidStone()',
+---				description = 'Check if the item is Solid Stone.',
+---			},
+---		}
+---	)
+--- ```
 ---@param functionNames string|string[] If the function has no aliases, just pass its name as a string, or to provide aliases, pass a list of strings (see notes above).
 ---@param ruleFunction function The actual function (see notes above).
----@param environmentVariables table<string,string>? List of variables to add to the rule environment. See the BagType rule in Config\RuleFunctions.lua for an example.
+---@param environmentVariables table<string,string>? List of variables to add to the rule environment. Will usually be nil. See the BagType rule in Config\RuleFunctions.lua for an example.
 ---@param ruleTemplates { code: string, description: string }[]? List of examples for use in the Category Editor rule function menu (see notes above).
 function Bagshui:AddRuleFunction(functionNames, ruleFunction, environmentVariables, ruleTemplates)
 	BsRules:AddFunction(functionNames, ruleFunction, environmentVariables, ruleTemplates)
