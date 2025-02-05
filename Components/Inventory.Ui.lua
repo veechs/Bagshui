be30cb3df7a7b73c0081a13c32826eb8e418d151
veechs <129508963+veechs@users.noreply.github.com>
2025-02-05 18:22:54 -0500

    Avoid resetting window position while dragging
    Fix #69.

--- Components/Inventory.Ui.lua
+++ Components/Inventory.Ui.lua
@@ -1034,6 +1034,7 @@ function Inventory:UiFrame_OnDragStart()
 		Bagshui.components[self.dockTo]:UiFrame_OnDragStart()
 	elseif not self.settings.windowLocked then
 		-- Has to be self instead of "this" because it can be called from docked windows.
+		self.dragInProgress = true
 		self.uiFrame:StartMoving()
 	end
 end
@@ -1046,6 +1047,7 @@ function Inventory:UiFrame_OnDragStop()
 		Bagshui.components[self.dockTo]:UiFrame_OnDragStop()
 	elseif not self.settings.windowLocked then
 		-- Has to be self instead of "this" because it can be called from docked windows.
+		self.dragInProgress = false
 		self.uiFrame:StopMovingOrSizing()
 		self:FixSettingsMenuPosition()
 		self:SaveWindowPosition()
@@ -1175,6 +1177,9 @@ end
 --- Keep the window in the right place.
 ---@param noRescueAttempts boolean? Don't call `Inventory:RescueWindow()`.
 function Inventory:FixWindowPosition(noRescueAttempts)
+	if self.dragInProgress then
+		return
+	end
 
 	-- As soon as a frame is dragged, its anchor changes to TOPLEFT, which leads to undesirable behavior
 	-- when the frame is resized. We have to reset the anchor and position to keep things happy.
